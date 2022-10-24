d77733530e08ed0c9263902b9cde457b3adc358c
Nedd Chairi Muñoz <91211902+NeddM@users.noreply.github.com>
2022-10-24 23:46:26 +0200

    Add files via upload

---
+++ autoClicker.py
@@ -0,0 +1,31 @@
+# Nedd Chairi Muñoz
+# https://github.com/NeddM
+
+import pyautogui
+import keyboard
+
+print("""
+Pulsa "o" para parar el bot.
+Pulsa "i" para reanudar el bot.
+Pulsa "p" para salir del bot.
+""")
+
+def farmea():
+    while True:
+        pyautogui.tripleClick()
+        if keyboard.is_pressed('o'):
+            noFarmea()
+            continue
+        elif keyboard.is_pressed("p"):
+            break
+
+def noFarmea():
+    while True:
+        if keyboard.is_pressed('i'):
+            farmea()
+            continue
+        elif keyboard.is_pressed('p'):
+            break
+
+if __name__ == "__main__":
+    noFarmea()
